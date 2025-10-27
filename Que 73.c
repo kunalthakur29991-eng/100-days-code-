b2d3181c5fd08f6e31b9a80bc41b34b3e71b7ed2
kunalthakur29991-eng <kunalthakur29991@gmail.com>
2025-10-27 21:27:33 +0530

    Day 37

---
+++ Que 73.c
@@ -0,0 +1,34 @@
+//Q73: Find the sum of each row of a matrix and store it in an array.
+#include <stdio.h>
+ int main()
+  {
+    printf(" Name - Kunal thakur\n SAP id - 590028530\n Course - B.sc CS\n Batch - B1\n ");
+    int rows, cols;
+    scanf("%d %d", &rows, &cols);
+
+    int matrix[rows][cols];
+    int rowSum[rows];
+    for(int i = 0; i < rows; i++) 
+    {
+        for(int j = 0; j < cols; j++) 
+        {
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+
+    for(int i = 0; i < rows; i++)
+     {
+        rowSum[i] = 0;
+        for(int j = 0; j < cols; j++)
+         {
+            rowSum[i] += matrix[i][j];
+        }
+    }
+
+    for(int i = 0; i < rows; i++) 
+    {
+        printf("%d ", rowSum[i]);
+    }
+
+    return 0;
+}
