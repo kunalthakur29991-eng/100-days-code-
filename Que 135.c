e3316d5735c7f6b6be0c0cb477e752141abe57f5
kunalthakur29991-eng <kunalthakur29991@gmail.com>
2025-11-21 09:43:57 +0530

    Day 85

---
+++ Que 135.c
@@ -0,0 +1,18 @@
+//Q135: Assign explicit values starting from 10 and print them.
+#include <stdio.h>
+
+enum Letters {
+    A = 10,
+    B,
+    C
+};
+
+int main()
+ {
+     printf("Name - Kunal thakur\n Sap id - 590028530\n Course - B sc CS\n Batch - B1\n");
+    printf("A=%d\n", A);
+    printf("B=%d\n", B);
+    printf("C=%d\n", C);
+
+    return 0;
+}
