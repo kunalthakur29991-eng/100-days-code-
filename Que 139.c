12469b2274696efc1d37be13712e6583484e5e7c
kunalthakur29991-eng <kunalthakur29991@gmail.com>
2025-11-21 09:47:30 +0530

    Day 89

---
+++ Que 139.c
@@ -0,0 +1,37 @@
+//Q139: Show that enums store integers by printing assigned values.
+#include <stdio.h>
+#include <string.h>
+
+enum Role {
+    ADMIN,
+    USER,
+    GUEST
+};
+
+int main()
+ {
+     printf("Name - Kunal thakur\n Sap id - 590028530\n Course - B sc CS\n Batch - B1\n");
+    char input[10];
+    enum Role r;
+
+    scanf("%s", input);
+
+    if (strcmp(input, "ADMIN") == 0)
+        r = ADMIN;
+    else if (strcmp(input, "USER") == 0)
+        r = USER;
+    else if (strcmp(input, "GUEST") == 0)
+        r = GUEST;
+    else {
+        printf("Invalid role\n");
+        return 0;
+    }
+
+    switch (r) {
+        case ADMIN: printf("Welcome Admin!\n"); break;
+        case USER:  printf("Welcome User!\n");  break;
+        case GUEST: printf("Welcome Guest!\n"); break;
+    }
+
+    return 0;
+}
